2c4bcc07fd181b59aa3626642a7b52a30f943118
IngridAndreaa <ingrid.pauchard@inacapmail.cl>
2025-10-06 00:51:50 -0300

    Actualizacion

--- ProyectoHoyos/AplicacionPauchard/models.py
+++ ProyectoHoyos/AplicacionPauchard/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Alumno(models.Model):
+    rut = models.CharField(primary_key=True, max_length=12)
+    nombrecompleto = models.CharField(max_length=20)
+    carrera = models.CharField(max_length=25)
